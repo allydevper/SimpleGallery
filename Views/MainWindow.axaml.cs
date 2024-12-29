e0bf60789c126c62199c709dcc79e9a8309b28c0
allydevper <alamaswilmer@gmail.com>
2024-12-29 17:13:36 -0500

    Update MainWindow.axaml.cs

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -17,38 +18,77 @@ namespace SimpleGallery.Views
 
         private void OnFolderDoubleTapped(object? sender, RoutedEventArgs e)
         {
-            if (sender is Button button && button.DataContext is FolderFilesModel folder)
+            if (sender is Button button && button.DataContext is FolderFilesModel _folder)
             {
                 if (DataContext is MainWindowViewModel viewModel)
                 {
-                    viewModel.SelectFolderCommand.Execute(folder);
+                    viewModel.SelectFolderCommand.Execute(_folder);
                 }
             }
         }
 
         private void OnImageDoubleTapped(object? sender, RoutedEventArgs e)
         {
-            if (sender is Button button && button.DataContext is FolderFilesModel image)
+            if (sender is Button button && button.DataContext is FolderFilesModel _image)
             {
                 if (DataContext is MainWindowViewModel viewModel)
                 {
+                    var currentImageIndex = viewModel.Images.IndexOf(_image);
+
                     var fullScreenWindow = new Window
                     {
                         WindowState = WindowState.FullScreen,
-                        Background = Brushes.Black,
-                        Content = new Image
-                        {
-                            Source = new Bitmap(image.Path),
-                            Stretch = Stretch.Uniform
-                        }
+                        Background = Brushes.Black
                     };
 
-                    fullScreenWindow.KeyDown += (s, e) =>
+                    var mainPanel = new Grid();
+                    var image = new Image
                     {
-                        if (e.Key == Key.Escape) fullScreenWindow.Close();
+                        Source = new Bitmap(_image.Path),
+                        Stretch = Stretch.Uniform
                     };
 
-                    fullScreenWindow.PointerPressed += (s, e) => fullScreenWindow.Close();
+                    // Botón para cambiar imagen anterior
+                    var prevButton = new Button
+                    {
+                        Content = "<",
+                        VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+                        HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Left,
+                        Margin = new Thickness(20),
+                        FontSize = 30
+                    };
+
+                    prevButton.Click += (s, e) =>
+                    {
+                        currentImageIndex = (currentImageIndex - 1 + viewModel.Images.Count) % viewModel.Images.Count;
+                        image.Source = new Bitmap(viewModel.Images[currentImageIndex].Path);
+                    };
+
+                    // Botón para cambiar imagen siguiente
+                    var nextButton = new Button
+                    {
+                        Content = ">",
+                        VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+                        HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Right,
+                        Margin = new Thickness(20),
+                        FontSize = 30
+                    };
+
+                    nextButton.Click += (s, e) =>
+                    {
+                        currentImageIndex = (currentImageIndex + 1) % viewModel.Images.Count;
+                        image.Source = new Bitmap(viewModel.Images[currentImageIndex].Path);
+                    };
+
+                    // Manejar el cierre con doble clic
+                    image.DoubleTapped += (s, e) => fullScreenWindow.Close();
+
+                    // Agrega los elementos al panel
+                    mainPanel.Children.Add(image);
+                    mainPanel.Children.Add(prevButton);
+                    mainPanel.Children.Add(nextButton);
+
+                    fullScreenWindow.Content = mainPanel;
                     fullScreenWindow.Show();
                 }
             }
